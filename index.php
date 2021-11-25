8d1df16ff5904115b8cac1db57d16eaab4be7a68
Justin-De-Sio <desio.j@live.fr>
2021-11-25 23:14:37 +0100

    update.php

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ start_page('OUI');
 echo date('d/m/Y',strtotime('2001-03-12')).'<br/>';
 echo date(' F d, Y, g:i a ',strtotime('2001-03-12'));
 ?>
+<p><a href="update.php"></a></p>
 
-<a href="update.php"></a>
 
 
 end_page();
