7622b17ddaa96024320f60c3707b9bac18201fb8
Justin-De-Sio <desio.j@live.fr>
2021-11-25 18:27:13 +0100

    date

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'Hello World!';
+include 'start_end.php';
+
+start_page('OUI');
+
+echo date('d/m/Y',strtotime('2001-03-12')).'<br/>';
+echo date(' F d, Y, g:i a ',strtotime('2001-03-12'));
+
+
+end_page();
+
---
+++ start_end.php
@@ -0,0 +1,20 @@
+<?php
+function start_page($titre)
+{
+?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head><title><?php echo $titre; ?></title></head>
+    <body>
+<?php
+}
+?>
+<?php
+function end_page(){
+    ?>
+    </body>
+    </html>
+<?php
+}
+?>
+
