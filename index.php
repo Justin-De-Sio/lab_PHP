3b169ac9bbdeed1ce1a7dfe713ba5ed4dfc018a0
superjustin <superjustin@ssh1.paris1.alwaysdata.com>
2021-11-26 11:06:29 +0100

    je viens de rajouter une balise test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo date('d/m/Y',strtotime('2001-03-12')).'<br/>';
 echo date(' F d, Y, g:i a ',strtotime('2001-03-12'));
 ?>
 <p><a href="update.php">UPDATE</a></p>
-
+<p>test</p>
 <p>zz</p>
 <?php
 end_page();
