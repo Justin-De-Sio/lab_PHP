f1d27e482608830a73f7e0bda9740e2d9fd4e5c2
Justin-De-Sio <desio.j@live.fr>
2021-12-02 15:52:40 +0100

    sommaire

--- TD1/index.php
+++ TD1/index.php
@@ -5,6 +5,6 @@ start_page('OUI');
 
 echo date('d/m/Y',strtotime('2001-03-12')).'<br/>';
 echo date(' F d, Y, g:i a ',strtotime('2001-03-12'));
-
+echo '<a href="calculator.php">Calculator</a>';
 end_page();
 
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include  'TD2/utils.inc.php';
+start_page('Sommaire');
+?>
+<h1>Sommaire</h1>
+<ul>
+    <li><a href="TD1/index.php">TD1</a></li>
+    <li><a href="TD2/index.php">TD2</a></li>
+</ul>
+<?php
+end_page();
+?>
