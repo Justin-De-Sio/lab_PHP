83ce416b9698cd8a8ad8714c199daeb9ca617e5c
Justin-De-Sio <desio.j@live.fr>
2021-12-04 12:16:25 +0100

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ start_page('Sommaire');
 <ul>
     <li><a href="TD1/index.php">TD1</a></li>
     <li><a href="TD2/index.php">TD2</a></li>
-    <li>SALUT</li>
+    <li>SALUT2</li>
 </ul>
 <?php
 end_page();
