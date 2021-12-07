27d7b9c2fe995f5c9d38fe14600bdc791e4dd8ce
Justin-De-Sio <desio.j@live.fr>
2021-12-07 11:20:05 +0100

    update

--- TD2/data-processing.php
+++ TD2/data-processing.php
@@ -2,21 +2,21 @@
 $action = $_POST['action'];
 $email = $_POST['user_email'];
 $password = $_POST['user_password'];
-if(!empty( $_POST['action'])){
-    if ($action == 'mailer') {
-        $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
-        $message .= 'Email : ' . $email . PHP_EOL;
-        $message .= 'Mot de passe : '  . $password. PHP_EOL;
 
-        $to = $email;
-        $subject = 'le sujet';
+if ($action == 'mailer') {
+    $message = 'Voici vos identifiants d\'inscription :' . PHP_EOL;
+    $message .= 'Email : ' . $email . PHP_EOL;
+    $message .= 'Mot de passe : ' . $password . PHP_EOL;
+
+    $to = $email;
+    $subject = 'le sujet';
 
 
-        mail($to, $subject, $message);
-        echo '<br/>email bien envoyé !<br/>';
-    } else {
-        echo '<br/><strong>Bouton non géré !</strong><br/>';
-    }
+    mail($to, $subject, $message);
+    echo '<br/>email bien envoyé !<br/>';
+} else {
+    echo '<br/><strong>Bouton non géré !</strong><br/>';
+
 
 }
 
