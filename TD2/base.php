f48270ad396a7ed2cda2e14b3de48420dc133bb9
Justin-De-Sio <desio.j@live.fr>
2021-12-09 18:51:48 +0100

    update

--- TD2/base.php
+++ TD2/base.php
@@ -1,39 +1,30 @@
 <?php
-$dbLink = mysqli_connect('mysql-superjustin.alwaysdata.net', '250372', 'U1234567890U',
-    'superjustin_td_php')
-or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
-?>
+function connectDB($query){
+    $dbLink = mysqli_connect('mysql-superjustin.alwaysdata.net', '250372', 'U1234567890U',
+        'superjustin_td_php')
+    or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+
+    mysqli_select_db($dbLink, 'superjustin_td_php')
+    or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink));
 
 
-<?php
-mysqli_select_db($dbLink, 'superjustin_td_php')
-or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
-);
-
-?>
-<?php
-$query = "INSERT INTO user (date, email, id, state, password, phone,sex) VALUES ( '$today',
-     '$email', '$id' , '$state' , '$password' , '$phone' ,'$sex' )";
-?>
-
-<?php
-if(!($dbResult = mysqli_query($dbLink, $query)))
-{
-    echo 'Erreur de requête<br/>';
-    // Affiche le type d'erreur.
-    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
-    // Affiche la requête envoyée.
-    echo 'Requête : ' . $query . '<br/>';
-    exit();
+    if(!($dbResult = mysqli_query($dbLink, $query))) {
+        echo 'Erreur de requête<br/>';
+        // Affiche le type d'erreur.
+        echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+        // Affiche la requête envoyée.
+        echo 'Requête : ' . $query . '<br/>';
+        exit();
+    }
 }
-?>
 
 
-<?php
-while($dbRow = mysqli_fetch_assoc($dbResult))
+
+
+
+/*while($dbRow = mysqli_fetch_assoc($dbResult))
 {
     echo $dbRow['id'] . '<br/>';
     echo $dbRow['email'] . '<br/>';
     echo $dbRow['date'] . '<br/>';
-    echo '<br/><br/>';
- }
+    echo '<br/><br/>';*/
