fb0d40b18e978d4a610244de9d55b9dcb0f80835
Justin-De-Sio <desio.j@live.fr>
2021-12-09 14:33:29 +0100

    update

--- TD2/base.php
+++ TD2/base.php
@@ -4,7 +4,21 @@ or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
 ?>
     Sélectionnez ensuite votre base de données :
 <?php
+$query='SELECT * FROM `user` ';
+
 mysqli_select_db($dbLink , 'superjustin_td_php	')
 or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
 );
 ?>
+<?php
+if(!($dbResult = mysqli_query($dbLink, $query)))
+{
+    echo 'Erreur dans requête<br />';
+    // Affiche le type d'erreur.
+    echo 'Erreur : ' . mysqli_error($dbLink) . '<br/>';
+    // Affiche la requête envoyée.
+    echo 'Requête : ' . $query . '<br/>';
+    exit();
+}
+?>
+
