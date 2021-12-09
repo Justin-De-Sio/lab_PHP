f5442f40b7ba2729d683f7d30936ddf9351aa4c6
Justin-De-Sio <desio.j@live.fr>
2021-12-09 14:27:55 +0100

    update

---
+++ TD2/base.php
@@ -0,0 +1,10 @@
+<?php
+$dbLink = mysqli_connect('mysql-superjustin.alwaysdata.net', '250372', 'lol1234567890lol!')
+or die('Erreur de connexion au serveur : ' . mysqli_connect_error());
+?>
+    Sélectionnez ensuite votre base de données :
+<?php
+mysqli_select_db($dbLink , 'superjustin_td_php	')
+or die('Erreur dans la sélection de la base : ' . mysqli_error($dbLink)
+);
+?>
