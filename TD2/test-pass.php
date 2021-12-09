7468b312d037d50987199c9937770700014f9ce2
Justin-De-Sio <desio.j@live.fr>
2021-12-09 22:37:29 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -10,9 +10,14 @@ if (!empty($_POST)) {
         echo $query;
         $dbResult = connectDB($query);
         $dbRow = mysqli_fetch_assoc($dbResult);
-       /* if (empty($dbRow)){
-            header('Location : welcome.php');
-        }*/
+        if (empty($dbRow)){
+            /* Redirection vers une page différente du même dossier */
+            $host  = $_SERVER['HTTP_HOST'];
+            $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+            $extra = 'welcome.php';
+            header("Location: http://$host$uri/$extra");
+            exit;
+
 
 
 
