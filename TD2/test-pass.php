c56a0a91ddaa96719c294fe0ef88cead3cdb8f79
Justin-De-Sio <desio.j@live.fr>
2021-12-10 15:10:26 +0100

    enfin !

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -5,7 +5,7 @@ if (!empty($_POST)) {
 
     $login = $_POST['login'];
 
-    $dbRow = mysqli_fetch_assoc($dbResult = connectDB($query = "select * from user where id='$login'"));
+    $dbRow = mysqli_fetch_assoc($dbResult = connectDB($query = 'select * from user where id=\'$login\''));
 
     if ($dbRow['id'] == $_POST['login'] && $_POST['password'] == $dbRow['password']) {
         session_start();
