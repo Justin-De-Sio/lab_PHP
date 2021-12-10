6d794352e98700b0c67e3dab03c2c166d60972fe
Justin-De-Sio <desio.j@live.fr>
2021-12-10 13:55:08 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -24,7 +24,7 @@ if (!empty($_POST)) {
 
         } else {
 
-//                header('location: index.php');
+                header('location: index.php');
 //                echo '<strong>logging ou mot de passe incorrect</strong>';
         }
     }
