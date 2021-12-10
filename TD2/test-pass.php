299721c5d8b2596c87919c5a8edcd73703af3402
Justin-De-Sio <desio.j@live.fr>
2021-12-10 13:47:55 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -4,24 +4,18 @@ if (!empty($_POST)) {
     $login = $_POST['login'];
     $password = $_POST['password'];
 
-    $query = "SELECT ID, PASSWORD FROM user ";
-    $dbResult = connectDB($query);
-    $dbRow = mysqli_fetch_assoc($dbResult);
 
-    while($dbRow = mysqli_fetch_assoc($dbResult)) {
-        echo $dbRow['id'] . '<br/>';
-        echo $dbRow['password'] . '<br/>';
-        echo '<br/><br/>';
-    }
+
 
     if (!empty($_POST)) {
 
 
-        $loginBD = $dbRow['id'];
-        $passwordBD = $dbRow['password'];
+        $dbRow = mysqli_fetch_assoc($dbResult = connectDB($query = 'select * from user'));
+        $loginDB = $dbRow['id'];
+        $passwordDB = $dbRow['password'];
 
 
-        if ($loginBD == $_POST['login'] && password_verify($_POST['password'], $passwordBD)) {
+        if ($loginDB == $_POST['login'] && password_verify($_POST['password'], $passwordDB)) {
             session_start();
             $_SESSION['suid'] = session_id();
             header('location: welcome.php');
