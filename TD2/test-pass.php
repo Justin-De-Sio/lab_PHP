43bf29dd882468bd4c09ff78d41c28da0f0ff3ca
Justin-De-Sio <desio.j@live.fr>
2021-12-10 09:42:29 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -3,36 +3,22 @@ require 'base.php';
 if (!empty($_POST)) {
     $login = $_POST['login'];
     $password = $_POST['password'];
-    $query = "SELECT ID, PASSWORD FROM user WHERE ID='$login'AND PASSWORD='$password'";
-    $dbResult = connectDB($query);
-    $dbAssoc = mysqli_fetch_assoc($dbResult);
-    echo $dbAssoc;
-
-    if (filter_input(INPUT_POST, 'login') && filter_input(INPUT_POST, 'password')) {
-        $query = "SELECT ID, PASSWORD FROM user WHERE ID='$login'AND PASSWORD='$password'";
-        $dbResult = connectDB($query);
-        $dbAssoc = mysqli_fetch_assoc($dbResult);
-        $login = $dbAssoc['id'];
-        $password = $dbAssoc['password'];
 
 
-        if ($login == $_POST['login'] && password_verify($_POST['password'], $password)) {
-            session_start();
-            $_SESSION['suid'] = session_id();
-            header('location: welcome.php');
-        } else {
-            start_page('erreur');
-            echo "<strong>logging ou mot de passe incorrect</strong> , '$login' , '$password','$dbAssoc '";
-            var_dump($dbResult);
-            if (mysqli_num_rows($dbResult) != 0)
-            {
-                while ($row = mysqli_fetch_assoc($dbResult))
-                {
-                    echo $row['username'];
-                }
+    if (!empty($_POST)) {
+
+            $query = "SELECT ID, PASSWORD FROM user WHERE ID='$login'AND PASSWORD='$password'";
+            $dbResult = connectDB($query);
+//            $dbRow = mysqli_fetch_assoc($dbResult);
+            if (!empty($dbResult)) {
+                session_start();
+                $_SESSION['suid'] = session_id();
+                header('location: welcome.php');
+
+            } else {
+                header('location: index.html');
+                echo '<strong>logging ou mot de passe incorrect</strong>';
             }
-
         }
-    }
 
 }
