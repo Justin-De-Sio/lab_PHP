1c0a17a95919b8eb3da999f9043b88abd1307f4f
Justin-De-Sio <desio.j@live.fr>
2021-12-10 15:16:58 +0100

    enfin !

--- TD2/login.php
+++ TD2/login.php
@@ -2,8 +2,9 @@
 require '../utils.inc.php';
 require 'test-pass.php';
 start_page('page de connection');
-
+echo $_SESSION['error'];
 ?>
+
 <form action="login.php" method="post">
     <label>Login <input type="text" name="login"></label><br>
     <label>password <input type="password" name="password"></label>
