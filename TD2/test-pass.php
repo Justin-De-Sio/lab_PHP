bff3ff8f6bcfe07815da2bbe3c13ddc51ae27fa3
Justin-De-Sio <desio.j@live.fr>
2021-12-10 13:19:30 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -7,23 +7,23 @@ if (!empty($_POST)) {
 
     if (!empty($_POST)) {
 
-            $query = "SELECT ID, PASSWORD FROM user WHERE ID='$login'AND PASSWORD='$password'";
-            $dbResult = connectDB($query);
-            $dbRow = mysqli_fetch_assoc($dbResult);
+        $query = "SELECT ID, PASSWORD FROM user WHERE ID='$login'AND PASSWORD='$password'";
+        $dbResult = connectDB($query);
+        $dbRow = mysqli_fetch_assoc($dbResult);
 
 
-            echo '<br>'.'id :'.'$login' .'mdp :' .'<br>'. $password;
-            if ($login == $_POST['login'] && password_verify($_POST['pwd'], $password)) {
-                session_start();
-                $_SESSION['suid'] = session_id();
-                header('location: welcome.php');
+        echo '<br>' . 'id :' . '$login' . '<br>' . ' mdp : ' . $password;
+        if ($login == $_POST['login'] && password_verify($_POST['pwd'], $password)) {
+            session_start();
+            $_SESSION['suid'] = session_id();
+            header('location: welcome.php');
 
-            } else {
-                $loginBD = $dbRow['id'] ;
-                $login = $dbRow['password'] ;
+        } else {
+            $loginBD = $dbRow['id'];
+            $login = $dbRow['password'];
 //                header('location: index.php');
 //                echo '<strong>logging ou mot de passe incorrect</strong>';
-            }
         }
+    }
 
 }
