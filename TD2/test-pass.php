beab36748d2628b9d99ac2a17eada23ae30db3d0
Justin-De-Sio <desio.j@live.fr>
2021-12-10 13:28:10 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -12,7 +12,7 @@ if (!empty($_POST)) {
         $dbRow = mysqli_fetch_assoc($dbResult);
         $loginBD = $dbRow['id'];
         $passwordBD = $dbRow['password'];
-
+        echo $query;
         echo '<br>' . 'dbRow : ' .$dbRow;
         echo '<br>' . 'idBD : ' . $loginBD . '<br>' . ' mdpBD : ' . $passwordBD;
         echo '<br>' . 'id : ' . $login . '<br>' . ' mdp : ' . $password;
