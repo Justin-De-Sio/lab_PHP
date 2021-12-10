06a9f68233bbf2bccd7713d259973fadd980c156
Justin-De-Sio <desio.j@live.fr>
2021-12-10 16:21:50 +0100

    update

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 require 'utils.inc.php';
 start_page('Sommaire');
 $rub1 = 'calculator';
-$rub1addr= 'TD1/calculator.php';
-$rub2 = 'TD2';
-$rub2addr ='TD2/index.php';
-$rub3 = 'TD3';
-$rub3addr ='TD3/index.php';
+$rub1addr = 'TD1/calculator.php';
+
+$rub2form = 'TD2 Form';
+$rub2formAddr = 'TD2/form.php';
+$rub2signIn = 'TD2 Sign in';
+$rub2signInAddr = 'TD2/login.php';
+
 
 ?>
 
-<h1>Sommaire</h1>
-<ul>
-    <li><a href="<?php echo $rub1addr ?>"><?php echo $rub1 ?></a></li>
-    <li><a href="<?php echo $rub2addr ?>"><?php echo $rub2 ?></a></li>
-    <li><a href="<?php echo $rub3addr ?>"><?php echo $rub3 ?></a></li>
-</ul>
+    <h1>Sommaire</h1>
+    <ul>
+        <li><a href="<?php echo $rub1addr ?>"><?php echo $rub1 ?></a></li>
+        <li><a href="<?php echo $rub2formAddr ?>"><?php echo $rub2form ?></a></li>
+        <li><a href="<?php echo $rub2signInAddr ?>"><?php echo $rub2signIn ?></a></li>
+    </ul>
 
 
 <?php
