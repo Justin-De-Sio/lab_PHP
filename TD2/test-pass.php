9c5bfab9b6270984786b73c1934f2ba08782725d
Justin-De-Sio <desio.j@live.fr>
2021-12-10 13:50:09 +0100

    update

--- TD2/test-pass.php
+++ TD2/test-pass.php
@@ -13,8 +13,8 @@ if (!empty($_POST)) {
         $dbRow = mysqli_fetch_assoc($dbResult = connectDB($query = 'select * from user'));
         $loginDB = $dbRow['id'];
         $passwordDB = $dbRow['password'];
-
-
+        echo $login;
+        echo  $passwordDB ;
         if ($loginDB == $_POST['login'] && password_verify($_POST['password'], $passwordDB)) {
             session_start();
             $_SESSION['suid'] = session_id();
