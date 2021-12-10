0f73f708f845888b3c45971746ba0329ac27c1a8
Justin-De-Sio <desio.j@live.fr>
2021-12-10 16:11:54 +0100

    td3

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $rub1addr= 'TD1/calculator.php';
 
 <h1>Sommaire</h1>
 <ul>
-    <li><a href="<?php $rub1addr ?>"><?php $rub1 ?></a></li>
+    <li><a href="<?php $rub1addr ?>"><?php echo $rub1 ?></a></li>
     <li><a href="TD2/index.php">TD2</a></li>
 </ul>
 
