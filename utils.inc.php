4cc33b879d4b8c38f36d45a5b53963bd4dc0714f
Justin-De-Sio <desio.j@live.fr>
2021-12-11 21:01:39 +0100

    update

--- utils.inc.php
+++ utils.inc.php
@@ -21,12 +21,25 @@ $rub2signInAddr = '/TD2/login/login.php';
 
 ?>
 
-<h1>Sommaire</h1>
-<ul>
-    <li><a href="<?php echo $rub1addr ?>"><?php echo $rub1 ?></a></li>
-    <li><a href="<?php echo $rub2formAddr ?>"><?php echo $rub2form ?></a></li>
-    <li><a href="<?php echo $rub2signInAddr ?>"><?php echo $rub2signIn ?></a></li>
-</ul>
+<div class="container">
+    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+        </a>
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+            <li><a href="<?php echo $rub1addr ?>" class="nav-link px-2 link-secondary"><?php echo $rub1 ?></a></li>
+            <li><a href="<?php echo $rub2formAddr ?>" class="nav-link px-2 link-dark"><?php echo $rub2form ?></a></li>
+            <li><a href="<?php echo $rub2signInAddr ?>" class="nav-link px-2 link-dark"><?php echo $rub2signIn ?></a></li>
+
+        </ul>
+
+        <div class="col-md-3 text-end">
+            <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" class="btn btn-primary">Sign-up</button>
+        </div>
+    </header>
+</div>
 
 
 <?php
