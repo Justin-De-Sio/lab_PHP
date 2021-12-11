7c2d46df448c67805179eca5c6253cd7a811fbca
Justin-De-Sio <desio.j@live.fr>
2021-12-11 20:40:05 +0100

    update

--- TD1/start_end.php
+++ TD1/start_end.php
@@ -10,6 +10,26 @@ function start_page($titre)
 </head>
 <body>
 <?php
+
+
+$rub1 = 'calculator';
+$rub1addr = 'TD1/calculator.php';
+
+$rub2form = 'Form';
+$rub2formAddr = '/TD2/register/form.php';
+$rub2signIn = 'Sign in';
+$rub2signInAddr = '/TD2/login/login.php';
+
+?>
+
+<h1>Sommaire</h1>
+<ul>
+    <li><a href="<?php echo $rub1addr ?>"><?php echo $rub1 ?></a></li>
+    <li><a href="<?php echo $rub2formAddr ?>"><?php echo $rub2form ?></a></li>
+    <li><a href="<?php echo $rub2signInAddr ?>"><?php echo $rub2signIn ?></a></li>
+</ul>
+<?php
+
 }
 ?>
 <?php
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 require 'utils.inc.php';
 start_page('Sommaire');
+
 $rub1 = 'calculator';
 $rub1addr = 'TD1/calculator.php';
 
 $rub2form = 'Form';
-$rub2formAddr = 'TD2/register/form.php';
+$rub2formAddr = '/TD2/register/form.php';
 $rub2signIn = 'Sign in';
-$rub2signInAddr = 'TD2/login/login.php';
+$rub2signInAddr = '/TD2/login/login.php';
 
 
 ?>
@@ -18,8 +19,6 @@ $rub2signInAddr = 'TD2/login/login.php';
         <li><a href="<?php echo $rub2formAddr ?>"><?php echo $rub2form ?></a></li>
         <li><a href="<?php echo $rub2signInAddr ?>"><?php echo $rub2signIn ?></a></li>
     </ul>
-
-
 <?php
 end_page();
 ?>
