de6d0c4cd934518c0c08907c22ec66707be107be
Justin-De-Sio <desio.j@live.fr>
2021-12-11 21:06:59 +0100

    update

--- utils.inc.php
+++ utils.inc.php
@@ -35,8 +35,8 @@ $rub2signInAddr = '/TD2/login/login.php';
         </ul>
 
         <div class="col-md-3 text-end">
-            <button type="button" class="btn btn-outline-primary me-2">Login</button>
-            <button type="button" class="btn btn-primary">Sign-up</button>
+            <button type="button" href="<?php echo $rub2signInAddr ?>" class="btn btn-outline-primary me-2">Login</button>
+            <button type="button" href="<?php echo $rub2formAddr ?>"  class="btn btn-primary">Sign-up</button>
         </div>
     </header>
 </div>
