40471bc7e755cef75f7bda6fd389ff776bb13d83
Justin-De-Sio <desio.j@live.fr>
2021-12-12 11:20:39 +0100

    PUSSSSSSSSSH

--- TD2/login/test-pass.php
+++ TD2/login/test-pass.php
@@ -4,13 +4,13 @@ if (!empty($_POST)) {
 
 
     $login = $_POST['login'];
-    $query = 'select * from superjustin_td_php.user where id=' . '\'' . $login . '\'';
+    $query = 'select * from superjustin_td_php.user where email=' . '\'' . $login . '\'';
     $dbRow = mysqli_fetch_assoc($dbResult = connectDB($query ));
 
     $query = 'UPDATE superjustin_td_php.user t SET t.conexion_counter = t.conexion_counter + 1 WHERE t.id =' . '\'' . $login . '\'';
     connectDB($query);
 
-    if ($dbRow['id'] == $_POST['login'] && $_POST['password'] == $dbRow['password']) {
+    if ($dbRow['email'] == $_POST['login'] && $_POST['password'] == $dbRow['password']) {
         session_start();
         $_SESSION['suid'] = session_id();
         header('location: welcome.php');
