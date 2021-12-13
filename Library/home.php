43f49388483b7c93567f111cf99f5efbe3fc4441
Justin-De-Sio <desio.j@live.fr>
2021-12-13 12:06:45 +0100

    update

--- Library/home.php
+++ Library/home.php
@@ -3,14 +3,14 @@ require 'Book.php';
 require 'Library.php';
 require '../utils.inc.php';
 start_page('library');
-echo '<h1>library</h1>'
+echo '<h1>library</h1>';
 $livre = new Book('Factulness', 'Hans Rosling', 'Flammarion', 400);
 
 $livre2 = new Book('toto', 'Hans rz', 'HYT', 23);
 
 $biblio = new Library('bioblio1','3 rue bobo',4);
 $biblio->addBook($livre);
-$biblio->addBook($livre2)
+$biblio->addBook($livre2);
 
 echo $biblio;
 
