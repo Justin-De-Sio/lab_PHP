52dae171e8fa3c97f0b0bebbc76a44d3a1c89be1
Justin-De-Sio <desio.j@live.fr>
2021-12-17 08:57:55 +0100

    ezae

--- TD2/base.php
+++ TD2/base.php
@@ -5,7 +5,7 @@ function connectDB($query)
     $user = '250372'    ;
     $pass= 'U1234567890U';
     try {
-        $db = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+        $db = new PDO('mysql:host=mysql-superjustin.alwaysdata.net;dbname=superjustin_td_php', $user, $pass);
 
     } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
@@ -17,8 +17,5 @@ function connectDB($query)
 
 
 }
-
-
-
 
 
